890bc5e304565cd1b70f0329976b30ef0e23fca7
Nuno Oliveira <nuno.oliveira@isel.pt>
2025-03-11 15:00:59 +0000

    update código aula

--- 01-forks1/mainForks2.c
+++ 01-forks1/mainForks2.c
@@ -20,6 +20,27 @@ int main ()
                 getpid(), getppid());
 
     pid_t retfork = fork();
+    if ( retfork == 0) {
+        // novo processo
+        printf("Child Process width pid = %d; ppid = %d\n", getpid(), getppid());
+
+        // Se apenas o filho executar o process_work o pai termina primeiro
+        // o que acontece ao processo filho?
+        process_work(DEFAULT_ITER);
+        
+        printf("Child Process terminating width pid = %d; ppid = %d\n", 
+                    getpid(), getppid());
+        return 0;
+    }
+
+    // Processo ? -> processo pai
+    printf("My pid = %d e create a process %d\n", getpid(), retfork);
+
+    // Se apenas o pai executar o process_work o filho termina primeiro
+    // o que acontece ao processo filho?
+    // process_work(DEFAULT_ITER);
+
+    printf("process %d terminating\n", getpid());
 
 
     printf("process %d terminating\n", getpid());
